aa8c9628f8ce91e977446b3fa3ee38cd640b2fc6
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2016-04-23 18:04:06 -0300

    Added secrets.yml to linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git@github.com:TristanPouliquen/IIC3103-Rails10.git'
 
 set :deploy_to, '/home/administrator/integra10-rails'
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
