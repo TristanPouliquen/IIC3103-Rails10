af50b379a84e6c9d08cef28f3403ea1e9f7254e1
swgaete <swgaete@uc.cl>
2016-05-01 16:29:39 -0300

    coloque el parametro id para almacén y producto

--- db/migrate/20160426173908_create_warehouses.rb
+++ db/migrate/20160426173908_create_warehouses.rb
@@ -1,6 +1,7 @@
 class CreateWarehouses < ActiveRecord::Migration
   def change
     create_table :warehouses do |t|
+      t.string  :id
       t.integer :usedSpace
       t.integer :totalSpace
       t.boolean :reception
--- db/migrate/20160426175318_create_products.rb
+++ db/migrate/20160426175318_create_products.rb
@@ -1,6 +1,7 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
+      t.string :id
       t.string :sku
       t.float :costos
       t.references :warehouse, index: true, foreign_key: true
