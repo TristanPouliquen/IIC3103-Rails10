6cdedf8d2318aba9d2c39ccdb0091c91126a5e48
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2016-05-02 11:09:53 -0300

    List product stock in each almacen

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,12 +17,25 @@ class PagesController < ApplicationController
       end
     end
 
-    @almacen_recepcion['porcentaje'] = (@almacen_recepcion["usedSpace"] / @almacen_recepcion["totalSpace"]) * 100
-    @almacen_despacho['porcentaje'] = (@almacen_despacho["usedSpace"] / @almacen_despacho["totalSpace"]) * 100
-    @almacen_pulmon['porcentaje'] = (@almacen_pulmon["usedSpace"] / @almacen_pulmon["totalSpace"]) * 100
+    @almacen_recepcion['porcentaje'] = ((@almacen_recepcion["usedSpace"].to_f / @almacen_recepcion["totalSpace"].to_f) * 100).round(2)
+    @almacen_despacho['porcentaje'] = ((@almacen_despacho["usedSpace"].to_f / @almacen_despacho["totalSpace"].to_f) * 100).round(2)
+    @almacen_pulmon['porcentaje'] = ((@almacen_pulmon["usedSpace"].to_f / @almacen_pulmon["totalSpace"].to_f) * 100).round(2)
+
+    @stock = getStockPorAlmacen
   end
 
   def getAlmacenes
     return get(ENV['bodega_system_url'] + 'almacenes', hmac = generateHash('GET'))
+  end
+
+  def getStockPorAlmacen
+    hmac = generateHash('GET' + ENV['almacen_recepcion'])
+    stock_recepcion = JSON.parse(get(ENV['bodega_system_url'] + 'skusWithStock?almacenId=' + ENV['almacen_recepcion'], hmac = hmac).body)
+    hmac = generateHash('GET' + ENV['almacen_despacho'])
+    stock_despacho = JSON.parse(get(ENV['bodega_system_url'] + 'skusWithStock?almacenId=' + ENV['almacen_despacho'], hmac = hmac).body)
+    hmac = generateHash('GET' + ENV['almacen_pulmon'])
+    stock_pulmon = JSON.parse(get(ENV['bodega_system_url'] + 'skusWithStock?almacenId=' + ENV['almacen_pulmon'], hmac = hmac).body)
+
+    return {'recepcion' => stock_recepcion, 'despacho' => stock_despacho, 'pulmon' => stock_pulmon}
   end
 end
