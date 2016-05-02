032ffc7ed8766f0d0325204e806db1aeeaa24eee
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2016-05-02 12:06:47 -0300

    Second call to bodega routes

--- app/controllers/bodega_controller.rb
+++ app/controllers/bodega_controller.rb
@@ -3,4 +3,11 @@ class BodegaController < ApplicationController
     return get(ENV['bodega_system_url'] + 'almacenes', hmac = generateHash('GET'))
   end
 
+  def getSkusWithStock(almacenId)
+    hmac = generateHash('GET' + almacenId.to_s)
+    uri = ENV['bodega_system_url'] + 'skusWithStock?almacenId=' + almacenId.to_s
+
+    return get(uri, hmac= hmac)
+  end
+
 end
