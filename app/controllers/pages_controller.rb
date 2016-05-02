a5cbbdb6b09f02957f63714e87f1dc100f9c1ac4
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2016-05-02 10:54:18 -0300

    Begin graphic interface

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,28 @@
+require 'json'
+
 class PagesController < ApplicationController
   def home
+  end
+
+  def warehouses
+    response = getAlmacenes
+    almacenes = JSON.parse(response.body)
+    almacenes.each do |almacen|
+      if almacen["recepcion"]
+        @almacen_recepcion = almacen
+      elsif almacen["despacho"]
+        @almacen_despacho = almacen
+      elsif almacen["pulmon"]
+        @almacen_pulmon = almacen
+      end
+    end
+
+    @almacen_recepcion['porcentaje'] = (@almacen_recepcion["usedSpace"] / @almacen_recepcion["totalSpace"]) * 100
+    @almacen_despacho['porcentaje'] = (@almacen_despacho["usedSpace"] / @almacen_despacho["totalSpace"]) * 100
+    @almacen_pulmon['porcentaje'] = (@almacen_pulmon["usedSpace"] / @almacen_pulmon["totalSpace"]) * 100
+  end
+
+  def getAlmacenes
+    return get(ENV['bodega_system_url'] + 'almacenes', hmac = generateHash('GET'))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   end
 
   root 'pages#home'
+  get 'bodegas' => 'pages#warehouses'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
