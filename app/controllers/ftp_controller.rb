a7844e6a94fb5777b0a5d9ef19584ce7b0fa5f99
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2016-05-03 19:01:42 -0300

    FTP controller

---
+++ app/controllers/ftp_controller.rb
@@ -0,0 +1,11 @@
+require 'net/ftp'
+class FtpController < ApplicationController
+  def index
+    # ftp = Net::FTP.new
+    # ftp.connect("mare.ing.puc.cl",22)
+    # ftp.login("ENV['usuario_ftp']","ENV['clave_ftp']")
+    # files=ftp.chdir("/pedidos")
+    # files=ftp.nlst("*.txt")
+    # ftp.getbinaryfile('nif.rb-0.91.gz', 'nif.gz', 1024)
+  end
+end
