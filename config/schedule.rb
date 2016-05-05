629a4b4ee9d56069953209868cc5bfda9bbce4cc
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2016-05-05 13:22:54 -0300

    Change whenever cron log output

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 # stdout is ignored, and stderror is sent normally for cron to take care of, which
 # probably means it is emailed to the user whose crontab whenever writes to
-set :output, "#{fetch(:shared_path)}/log/cron.log"
+set :output, "#/home/administrator/integra10-rails/shared/log/cron.log"
 
 every 4.hours do
   rake "ftp:process"
