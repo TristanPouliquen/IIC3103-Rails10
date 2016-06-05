4c9cb5edb9fae2ef74c1bceb126e47b994abcce6
TristanPouliquen <tristan.pouliquen@yahoo.fr>
2016-06-05 18:45:44 -0400

    Fix for whenever bundle location

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,4 @@
+env :PATH, ENV['PATH']
 # stdout is ignored, and stderror is sent normally for cron to take care of, which
 # probably means it is emailed to the user whose crontab whenever writes to
 set :output, "/home/administrator/integra10-rails/shared/log/cron.log"
