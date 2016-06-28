44098ef978ccddb20726cfbf4260a3d1c81d3057
Tristan Pouliquen <tristan.pouliquen@yahoo.fr>
2016-06-28 17:56:24 -0400

    Task and graph for daily stocks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < BodegaController
 
   def businessIntelligence
     # Saldo diario
-    saldo_diarios = SaldoDiario.all.pluck(:saldo, :date)
+    saldo_diarios = SaldoDiario.order(:date).all.pluck(:saldo, :date)
     @saldo_diarios = []
     saldo_diarios.each do |item|
       item = {'date' => item[1].strftime("%F"), "value" => item[0]}
@@ -14,6 +14,18 @@ class PagesController < BodegaController
 
     # Facturacion diaria
     @fac = getDailyTransaction
+
+    # Estado de las bodegas
+    @bodegas = getBodegasStatus
+
+    # Stocks diarios
+    stock_diarios = StockDiario.order(:date).all
+    @stock_diarios = []
+    stock_diarios.each do |item|
+      data = item.stock
+      data['date'] = item['date'].strftime("%F")
+      @stock_diarios << data
+    end
   end
 
   def dayTransactions
@@ -53,18 +65,22 @@ class PagesController < BodegaController
     @almacen['despacho']['porcentaje'] = ((@almacen['despacho']["usedSpace"].to_f / @almacen['despacho']["totalSpace"].to_f) * 100).round(2)
     @almacen['pulmon']['porcentaje'] = ((@almacen['pulmon']["usedSpace"].to_f / @almacen['pulmon']["totalSpace"].to_f) * 100).round(2)
 
-    @stock = getStockPorAlmacen
+    @stock = getStockPorAlmacen(@almacen)
   end
 
-  def getStockPorAlmacen
-    hmac = generateHash('GET' + ENV['almacen_recepcion'])
-    stock_recepcion = JSON.parse(get(ENV['bodega_system_url'] + 'skusWithStock?almacenId=' + ENV['almacen_recepcion'], hmac = hmac).body)
-    hmac = generateHash('GET' + ENV['almacen_despacho'])
-    stock_despacho = JSON.parse(get(ENV['bodega_system_url'] + 'skusWithStock?almacenId=' + ENV['almacen_despacho'], hmac = hmac).body)
-    hmac = generateHash('GET' + ENV['almacen_pulmon'])
-    stock_pulmon = JSON.parse(get(ENV['bodega_system_url'] + 'skusWithStock?almacenId=' + ENV['almacen_pulmon'], hmac = hmac).body)
+  def getStockPorAlmacen(almacen)
+    hmac = generateHash('GET' + almacen['recepcion']['_id'])
+    stock_recepcion = JSON.parse(get(ENV['bodega_system_url'] + 'skusWithStock?almacenId=' + almacen['recepcion']['_id'], hmac = hmac).body)
+    hmac = generateHash('GET' + almacen['despacho']['_id'])
+    stock_despacho = JSON.parse(get(ENV['bodega_system_url'] + 'skusWithStock?almacenId=' + almacen['despacho']['_id'], hmac = hmac).body)
+    hmac = generateHash('GET' + almacen['pulmon']['_id'])
+    stock_pulmon = JSON.parse(get(ENV['bodega_system_url'] + 'skusWithStock?almacenId=' + almacen['pulmon']['_id'], hmac = hmac).body)
+    hmac = generateHash('GET' + almacen['X']['_id'])
+    stock_X = JSON.parse(get(ENV['bodega_system_url'] + 'skusWithStock?almacenId=' + almacen['X']['_id'], hmac = hmac).body)
+    hmac = generateHash('GET' + almacen['Y']['_id'])
+    stock_Y = JSON.parse(get(ENV['bodega_system_url'] + 'skusWithStock?almacenId=' + almacen['Y']['_id'], hmac = hmac).body)
 
-    return {'recepcion' => stock_recepcion, 'despacho' => stock_despacho, 'pulmon' => stock_pulmon}
+    return {'recepcion' => stock_recepcion, 'despacho' => stock_despacho, 'pulmon' => stock_pulmon, 'X' => stock_X, 'Y' => stock_Y}
   end
 
   def getTransactions(fechaInicio, fechaFin)
@@ -100,5 +116,47 @@ class PagesController < BodegaController
     end
 
     return {'volume' => volume, 'amount' => amount}
+  end
+
+  def getBodegasStatus
+    almacenes_data = getAlmacenes
+
+    almacenes = {}
+
+    almacenes_data.each do |almacen|
+      if almacen['pulmon']
+        almacenes['pulmon'] = almacen
+      elsif almacen['recepcion']
+        almacenes['recepcion'] = almacen
+      elsif almacen['despacho']
+        almacenes['despacho'] = almacen
+      elsif almacenes['X'].nil?
+        almacenes['X'] = almacen
+      else
+        almacenes['Y'] = almacen
+      end
+    end
+
+    stocks = getStockPorAlmacen(almacenes)
+    dataProvider = {
+      'recepcion' =>formatData(almacenes, stocks, 'recepcion'),
+      'despacho' =>formatData(almacenes, stocks, 'despacho'),
+      'pulmon' =>formatData(almacenes, stocks, 'pulmon'),
+      'X' =>formatData(almacenes, stocks, 'X'),
+      'Y' =>formatData(almacenes, stocks, 'Y')
+    }
+  end
+
+  def formatData(almacenes, stocks, bodega)
+    almacen = almacenes[bodega]
+    stock = stocks[bodega]
+
+    data = [{'sku' => 'Vacio', 'stock' => almacen['totalSpace'] - almacen['usedSpace']}]
+
+    stock.each do |stockItem|
+      data << {'sku' => stockItem['_id'], 'stock' => stockItem['total']}
+    end
+
+    return data
   end
 end
